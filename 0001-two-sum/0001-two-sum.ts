e4b308a670e917f43a4110763ba7aa838d6279b9
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-01-03 19:09:02 +0000

    Time: 61 ms (74.73%), Space: 45.2 MB (43.99%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,9 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map[target-nums[i]] !== undefined) return [map[target-nums[i]], i]
+
+        map[nums[i]] = i
+    }
+};
