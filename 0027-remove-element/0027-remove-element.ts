fe4951ac40bc255737d495e09b6f298392b8fd66
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-01-07 21:45:56 +0000

    Time: 61 ms (34.59%), Space: 44 MB (45.98%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,10 +1,6 @@
 function removeElement(nums: number[], val: number): number {
-    
     for (let i = nums.length-1; i >= 0; i--) {
-        if (nums[i] === val) {
-            nums.splice(i, 1)
-        }
+        if (nums[i] === val) nums.splice(i, 1)
     }
-
     return nums.length
 };
