22b4b22abf5ec5d2def60c20533ee6bfd7e9ac91
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-05-15 11:15:35 +0900

    Time: 77 ms (5.73%), Space: 54.1 MB (7.51%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.ts
@@ -0,0 +1,20 @@
+function search(nums: number[], target: number): number {
+    let low = 0
+    let high = nums.length-1
+    let mid
+    
+    while (high >= low) {
+        mid = low + Math.floor((high - low) / 2)
+
+        if (nums[mid] == target)
+            return mid;
+ 
+        if (nums[mid] > target)
+            high = mid - 1;
+            
+        else 
+            low = mid + 1;
+    }
+
+    return -1
+};
