67961311e159ca8c45dfba6e9ddfa742317663ee
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-06-07 12:20:08 +0900

    Time: 72 ms (9.8%), Space: 53.3 MB (91.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,19 +1,18 @@
 function search(nums: number[], target: number): number {
+
+    // assuming sorted array
     let low = 0
-    let high = nums.length-1
+    let high = nums.length - 1
     let mid
-    
+
     while (high >= low) {
         mid = low + Math.floor((high - low) / 2)
+        
+        if (nums[mid] === target) return mid
 
-        if (nums[mid] == target)
-            return mid;
- 
-        if (nums[mid] > target)
-            high = mid - 1;
-            
-        else 
-            low = mid + 1;
+        if (nums[mid] > target) high = mid - 1
+
+        else low = mid + 1
     }
 
     return -1
