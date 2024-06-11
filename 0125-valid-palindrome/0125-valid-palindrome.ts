7e006eff60dfaa7859138f2a855876129863cd02
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-06-11 18:49:26 +0900

    Time: 62 ms (80.8%), Space: 54.2 MB (44.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,7 +1,4 @@
 function isPalindrome(s: string): boolean {
-    const str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-
-    if (str === str.split('').reverse().join('')) return true
-
-    return false
+    const str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+    return str === str.split('').reverse().join('')
 };
