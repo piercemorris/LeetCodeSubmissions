52177233e7055b84e1ef36ad2febf3226e018c01
Pierce James Morris <17671699+piercemorris@users.noreply.github.com>
2024-07-07 15:42:29 +0900

    Time: 82 ms (18.38%), Space: 59.2 MB (48.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.ts
+++ 0011-container-with-most-water/0011-container-with-most-water.ts
@@ -4,12 +4,8 @@ function maxArea(height: number[]): number {
     let l = 0
     
     while (l < r) {
-        const area = Math.min(height[l], height[r]) * (r - l)
-        maxArea = Math.max(area, maxArea)
-
-        if (height[l] < height[r]) l++
-        else r--
-
+        maxArea = Math.max(Math.min(height[l], height[r]) * (r - l), maxArea)
+        if (height[l] < height[r]) { l++ } else r--
     }
 
     return maxArea
